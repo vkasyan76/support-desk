1e815520d3e3637fea383ac4b296622d32e70158
vkasyan76 <vkasyan@gmx.de>
2022-07-03 13:48:41 +0200

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('Server...')
+// console.log('Server...')
+
+const express = require('express')
+const dotenv = require('dotenv').config()
+    // const PORT = 5000
+const PORT = process.env.PORT || 500
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.listen(PORT, () => console.log(`Server startted on PORT ${PORT}`))
