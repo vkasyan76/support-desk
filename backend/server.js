a68e0880639d3687bef422c6cbd38c8ef9c26359
vkasyan76 <vkasyan@gmx.de>
2022-07-24 13:44:48 +0200

    Preperation to deplyoy

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
     // Set build folder as static:
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) =>
+    app.get('*', (_, res) =>
         res.sendFile(__dirname, '../frontend/build/index.html'),
     )
 } else {
